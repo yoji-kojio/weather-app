ddbffe8c33db6e780dc7d5736af06c8ed3b7c9af
Yoji Kojio <yoji.kojio@gmail.com>
2020-07-16 22:11:26 -0300

    Add api key on a env variable

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const axios = require('axios')
+require('dotenv').config()
 
+const hostUrl = 'http://api.weatherstack.com/current'
+const params = {
+  access_key: process.env.WEATHER_API_KEY,
+  query: 'Sao Jose dos Campos'
+}
+
+axios.get(hostUrl, { params }).then(({ data }) => {
+  console.log(data.current)
+}).catch((error) => {
+  console.log(error)
+})
